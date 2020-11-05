a660ba406911cc4e82d6731772370723c397525d
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-05 15:18:33 +0400

    #6 updated parse function

--- src/chatbot/core.clj
+++ src/chatbot/core.clj
@@ -4,14 +4,8 @@
 
 (defn parse-input [input]
   (let [words (str/split input #" ")
-        num_of_words (count words)]
-    (loop [i 0
-           lower-cased-words []]
-       (if (< i num_of_words)
-         (let [lower-cased-word (str/lower-case (nth words i))]
-            (recur (inc i)
-                   (conj lower-cased-words lower-cased-word)))
-         lower-cased-words))))
+        lower-cased-words (map str/lower-case words)]
+    lower-cased-words))
 
 (defn main
   ""
