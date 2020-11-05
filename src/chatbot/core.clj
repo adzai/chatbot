ef2e604b352cbd1eead145156d08ddd2a102c796
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-05 15:13:22 +0400

    #6 parsing user input

--- src/chatbot/core.clj
+++ src/chatbot/core.clj
@@ -1,5 +1,17 @@
 (ns chatbot.core
-  (:require [chatbot.get_data :as data]))
+  (:require [chatbot.get_data :as data]
+            [clojure.string :as str]))
+
+(defn parse-input [input]
+  (let [words (str/split input #" ")
+        num_of_words (count words)]
+    (loop [i 0
+           lower-cased-words []]
+       (if (< i num_of_words)
+         (let [lower-cased-word (str/lower-case (nth words i))]
+            (recur (inc i)
+                   (conj lower-cased-words lower-cased-word)))
+         lower-cased-words))))
 
 (defn main
   ""
