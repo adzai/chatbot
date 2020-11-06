9d15ebe5095da008d823cee7bab57c8223ff26d8
Adam Zaiter <zaiter.adam7@gmail.com>
2020-11-06 08:53:38 +0100

    #16: add test for similarity

--- test/chatbot/core_test.clj
+++ test/chatbot/core_test.clj
@@ -2,12 +2,24 @@
   (:require [clojure.test :refer :all]
             [chatbot.core :refer :all]
             [clojure.java.io :as io]
-            [chatbot.get_data :as data]))
+            [chatbot.get_data :refer [create-data]]
+            [chatbot.levenshtein :refer [similarity]]))
 
 (deftest data-test
   (testing "JSON file in data folder"
-    (data/create-data)
+    (create-data)
     (is
       (= true 
          (.exists (io/file "data/data-cz.json"))))))
-    
+
+(deftest test-parse-function 
+  (testing "Testing parse function")   
+  (is
+    (= (list "my" "name" "is" "ani")
+       (parse-input "My Name Is ANI"))))
+
+(deftest similarity-test
+  (testing "Testing similarity between two strings"
+    (is
+        (== 0.8 
+           (similarity "hello" "helo")))))
