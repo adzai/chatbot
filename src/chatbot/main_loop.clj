286daa5ee7979516ca96d73db92ec8cd0d19ab5e
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-11-13 12:15:05 +0100

    Removing the do statement
    This statement is redundant

--- src/chatbot/main_loop.clj
+++ src/chatbot/main_loop.clj
@@ -3,6 +3,6 @@
   []
   (println "Chatbot> Hi!")
   (println "Chatbot> I am your park guide. I will tell you about Bertramka park. To end the conversation, enter 'finish'. Ask your questions")
-  (loop [user-input (do (read-line))]
+  (loop [user-input (read-line)]
     (when-not (= "finish" user-input)
-      (recur (do (read-line))))))
+      (recur (read-line)))))
