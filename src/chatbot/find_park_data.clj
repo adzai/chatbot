bc7aa1bfee4a1d4448dd3f848f436e600887174c
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-11-14 22:50:50 +0100

    Changing argument name
    Argument "keyword" has been changed to "found-keyword" in order to avoid shadowing the built in Clojure procedure

--- src/chatbot/find_park_data.clj
+++ src/chatbot/find_park_data.clj
@@ -2,26 +2,26 @@
   "Checks what value of the keyword identified in user input is in the data
   structure containing data about the park and prints out the corresponding
   bot's answer depending on the keyword type"
-  [keyword]
-  (when-not (nil? (get park-map keyword))
+  [found-keyword]
+  (when-not (nil? (get park-map found-keyword))
     (cond
-      (some #(= keyword %) ["wc" "playground" "parking"])
-        (if (= "yes" (get (parse-json "data/synonyms.json") keyword))
-          (println (format ">Chatbot: You can find %s in Bertramka. " keyword))
-          (println (format ">Chatbot: Unfortunately, there is no %s in Bertramka. " keyword)))
+      (some #(= found-keyword %) ["wc" "playground" "parking"])
+        (if (= "yes" (get (parse-json "data/synonyms.json") found-keyword))
+          (println (format ">Chatbot: You can find %s in Bertramka. " found-keyword))
+          (println (format ">Chatbot: Unfortunately, there is no %s in Bertramka. " found-keyword)))
 
-      (some #(= keyword %) ["biking" "skating" "skiing"])
-      (if (= "yes" (get (parse-json "data/synonyms.json") keyword))
-        (println (format ">Chatbot: %s is possible in Bertramka. " keyword))
-        (println (format ">Chatbot: Unfortunately, %s is not possible in Bertramka. " keyword)))
+      (some #(= found-keyword %) ["biking" "skating" "skiing"])
+      (if (= "yes" (get (parse-json "data/synonyms.json") found-keyword))
+        (println (format ">Chatbot: %s is possible in Bertramka. " found-keyword))
+        (println (format ">Chatbot: Unfortunately, %s is not possible in Bertramka. " found-keyword)))
 
-      (= keyword "attractions")
-      (println (format ">Chatbot: In Bertramka you can find multiple attractions, some are: %s" (get (parse-json "data/synonyms.json") keyword)))
+      (= found-keyword "attractions")
+      (println (format ">Chatbot: In Bertramka you can find multiple attractions, some are: %s" (get (parse-json "data/synonyms.json") found-keyword)))
 
-      (= keyword "transportation")
-      (println (format ">Chatbot: You can get to Bertramka these ways:, they are: %s" (get (parse-json "data/synonyms.json") keyword)))
+      (= found-keyword "transportation")
+      (println (format ">Chatbot: You can get to Bertramka these ways:, they are: %s" (get (parse-json "data/synonyms.json") found-keyword)))
 
-      (= keyword "dogs")
-      (if (= "yes" (get (parse-json "data/synonyms.json") keyword))
+      (= found-keyword "dogs")
+      (if (= "yes" (get (parse-json "data/synonyms.json") found-keyword))
         (println ">Chatbot: You can enter Bertramka with your dogs")
         (println ">Chatbot:  Unfortunately, you can't enter Bertramka with your dogs")))))
