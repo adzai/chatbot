a233c5cc462975a2de9b9ad4444d70715bc6f847
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-11-14 22:43:14 +0100

    Substitute synonym map with parse-json
    Instead of using map of keywords and synonyms directly in the code, parse-json function is applied to the synonyms.json file containing synonym data

--- src/chatbot/find_park_data.clj
+++ src/chatbot/find_park_data.clj
@@ -1,14 +1,3 @@
-
-(def park-map {"wc" "yes",
-			         "attractions" "yes",
-			         "biking" "yes",
-               "skiing" "no",
-			         "skating" "no",
-			         "sports" "no",
-			         "playground" "no",
-			         "transportation" "yes",
-			         "parking" "yes"})
-
 (defn find-park-data 
   "Checks what value of the keyword identified in user input is in the data
   structure containing data about the park and prints out the corresponding
@@ -17,22 +6,22 @@
   (when-not (nil?(get park-map keyword))
     (cond
       (some #(= keyword %) ["wc" "playground" "parking"])
-        (if (= "yes" (get park-map keyword))
+        (if (= "yes" (get (parse-json "data/synonyms.json") keyword))
           (println (format ">Chatbot: You can find %s in Bertramka. " keyword))
           (println (format ">Chatbot: Unfortunately, there is no %s in Bertramka. " keyword)))
 
       (some #(= keyword %) ["biking" "skating" "skiing"])
-      (if (= "yes" (get park-map keyword))
+      (if (= "yes" (get (parse-json "data/synonyms.json") keyword))
         (println (format ">Chatbot: %s is possible in Bertramka. " keyword))
         (println (format ">Chatbot: Unfortunately, %s is not possible in Bertramka. " keyword)))
 
       (= keyword "attractions")
-      (println (format ">Chatbot: In Bertramka you can find multiple attractions, some are: %s" (get park-map keyword)))
+      (println (format ">Chatbot: In Bertramka you can find multiple attractions, some are: %s" (get (parse-json "data/synonyms.json") keyword)))
 
       (= keyword "transportation")
-      (println (format ">Chatbot: You can get to Bertramka these ways:, they are: %s" (get park-map keyword)))
+      (println (format ">Chatbot: You can get to Bertramka these ways:, they are: %s" (get (parse-json "data/synonyms.json") keyword)))
 
       (= keyword "dogs")
-      (if (= "yes" (get park-map keyword))
+      (if (= "yes" (get (parse-json "data/synonyms.json") keyword))
         (println ">Chatbot: You can enter Bertramka with your dogs")
         (println ">Chatbot:  Unfortunately, you can't enter Bertramka with your dogs")))))
