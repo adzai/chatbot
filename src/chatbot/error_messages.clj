f0c9726443a811dae07b66a46395e296387204a1
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-16 00:40:03 +0400

    error messages
    Vector of possible error messages which will be used in the main loop when the user input is not recognized.

---
+++ src/chatbot/error_messages.clj
@@ -0,0 +1,2 @@
+(def possible-error-messages 
+  (vector ">Chatbot: Sorry, I didn't understand you, please try again!" ">Chatbot: The Bot did not understand the question, please try in different words." ">Chatbot: I am sorry, I could not understand you"))
