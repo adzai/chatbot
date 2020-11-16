747024e99b4f4aa33011b9805c97dba7b9859a71
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-16 15:36:18 +0400

    Identify keyword
    "keyword-response-vector" identifies the keyword from the vector of synonyms. "keyword-response-list" runs it on the list of vectors and the last, main function executes them. The function returns true if the keyword is not identified.

---
+++ src/chatbot/identify_keyword.clj
@@ -0,0 +1,40 @@
+(def synonyms-map {"wc"  ["wc", "restroom", "bathroom", "toilet", "lavatory"],
+                   "attractions"  ["attractions", "sightseeing", "landmark", "entertainment"],
+                   "dogs"  ["dogs", "pet"],
+                   "biking"  ["biking", "bike", "bicycle"],
+                   "skiing"  ["skiing"],
+                   "skating"  ["skating", "roller", "skateboard", "rollerblade"],
+                   "sports"  ["sports", "gymnastics", "exercise"],
+                   "playground"  ["playground", "playing"],
+                   "transportation"  ["transportation", "metro", "subway", "tram", "bus", "transport"],
+                   "parking"  ["parking", "car"]})
+
+
+(defn keyword-response-vector  
+  "Takes the vector of synonyms, identifies keyword and prints the corresponding response. If the keyword is not identified, function returns true"
+  [synonyms-vector input]
+  (let [words (parse-input input)
+        synonyms synonyms-vector]
+  (loop [synonyms-vec synonyms-vector]
+    (let [synonym (first synonyms-vec)]
+      (if (not(nil? synonym)) 
+        (let [max-similarity (apply max (for [y words] (similarity y (first synonyms-vec))))]
+          (if (> max-similarity 0.75)
+            (find-park-data (first synonyms))
+            (recur (rest synonyms-vec))))
+        true)))))
+
+(defn keyword-response-list
+  "Takes the list of vectors containing synonyms and calls keyword-response function on the first vector. If the keyword is not found in any of the vectors, then returns true "
+  [synonyms-lst input]
+  (if (not (empty? synonyms-lst))
+    (if (= true (keyword-response-vector (first synonyms-lst) input))
+      (keyword-response-list (rest synonyms-lst) input))
+    true)) 
+
+(defn keyword-response-main 
+  "Parses the user-input and retrieves values from the synonyms-map and calls keyword-response-list on the synonyms list. "
+  [input]
+  (let [words (parse-input input)
+        synonyms (vals synonyms-map)]
+    (keyword-response-list synonyms input)))
