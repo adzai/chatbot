48b9d754331de8c26425595e8d6be2ef61b5d73b
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-17 15:51:10 +0400

    Updated line length

--- src/chatbot/greet.clj
+++ src/chatbot/greet.clj
@@ -5,10 +5,13 @@
   
 (def possible-greetings (vector "hey" "hi" "hello" "morning" "evening" "afternoon"))
 
-(def responses (vector ">Chatbot: Hello, thanks for visiting" ">Chatbot: Good to see you again" ">Chatbot: Hi there, how can I help?"))
+(def responses (vector ">Chatbot: Hello, thanks for visiting" 
+                       ">Chatbot: Good to see you again" 
+                       ">Chatbot: Hi there, how can I help?"))
 
 (defn greeting
-  "Using similarity function, identifies if the user-input is in possible-greetings, if true function prints random greeting message, otherwise retruns true."
+  "Using similarity function, identifies if the user-input is in possible-greetings.
+   If true, function prints random greeting message, otherwise retruns false."
   [greeting-vector input]
    (let [words (parse-input input)]
      (if (not (empty? greeting-vector))
