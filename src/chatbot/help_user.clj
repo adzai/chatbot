2cae22e1714f2869ff9ee500a437d4d7ae47ca51
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-17 16:14:09 +0400

    Updated line length

--- src/chatbot/help_user.clj
+++ src/chatbot/help_user.clj
@@ -1,4 +1,11 @@
 (defn help-function 
   "Describes the functionalities of Chatbot."
   []
-  (println "Chatbot> The chatbot is designed to answer users' questions regarding Bertramka park. The bot provides information about following aspects of Bertramka: wc, attractions, biking, skating, sports field, playground, transportation and parking. Error messages are used to infrom user that asked questions are obscure to chatbot.The user can finish the conversation by typing word - finish."))
+  (println "Chatbot> The chatbot is designed to answer users' 
+            questions regarding Bertramka park. 
+            The bot provides information about following aspects 
+            of Bertramka: wc, attractions, biking, skating, 
+            sports field, playground, transportation and parking. 
+            Error messages are used to infrom user that 
+            asked questions are obscure to chatbot.
+            The user can finish the conversation by typing word - finish."))
