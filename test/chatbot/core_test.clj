7ace16aa33a73921212f56e078d7680ec53ca5d7
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-17 20:50:35 +0400

    Test functions for identify keyword

--- test/chatbot/core_test.clj
+++ test/chatbot/core_test.clj
@@ -3,7 +3,8 @@
             [chatbot.core :refer :all]
             [clojure.java.io :as io]
             [chatbot.get_data :refer [create-data]]
-            [chatbot.levenshtein :refer [similarity]]))
+            [chatbot.levenshtein :refer [similarity]]
+            [chatbot.identify_keyword :refer :all]))
 
 (deftest data-test
   (testing "JSON file in data folder"
@@ -28,3 +29,33 @@
   (testing "Testing json-parse function"
     (is
       (map? (parse-json "data/data-en.json")))))
+
+(deftest keyword-response-vector-valid-test
+  (testing "Testing keyword identifier function with valid input"
+    (is 
+      (= "wc" (keyword-response-vector (vector "wc" "restroom" "bath") "restroom")))))
+
+(deftest keyword-response-vector-invalid-test
+  (testing "Testing keyword identifier function with invalid input"
+    (is 
+      (= false (keyword-response-vector (first (vals synonyms-map)) "something")))))
+
+(deftest keyword-response-list-valid-test
+  (testing "Testing keyword identifier with list of vectors and invalid input"
+    (is
+      (= "transportation" (keyword-response-list (vals synonyms-map) "metro")))))
+
+(deftest keyword-response-list-invalid-test
+  (testing "Testing keyword identifier with list of vectors and invalid input"
+    (is
+      (= false (keyword-response-list (vals synonyms-map) "something")))))
+
+(deftest keyword-response-main-valid-test
+  (testing "Testing the keyword identifier function with valid input"
+    (is
+      (= "biking" (keyword-response-main "bicycle")))))
+
+(deftest keyord-response-main-invalid-test
+  (testing "Testing the keyword identifier function with invalid input"
+    (is
+      (= false (keyword-response-main "Something")))))
