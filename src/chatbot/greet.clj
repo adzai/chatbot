bd37a90bb3025ec5544c34b498c3c55aaa373d38
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-17 16:36:11 +0400

    Added newlines into the vectors and functions

--- src/chatbot/greet.clj
+++ src/chatbot/greet.clj
@@ -3,19 +3,26 @@
             [chatbot.levenshtein :refer [similarity]]))
   
   
-(def possible-greetings (vector "hey" "hi" "hello" "morning" "evening" "afternoon"))
+(def possible-greetings 
+  (vector "hey" "hi" "hello" "morning" "evening" "afternoon"))
 
-(def responses (vector ">Chatbot: Hello, thanks for visiting" 
-                       ">Chatbot: Good to see you again" 
-                       ">Chatbot: Hi there, how can I help?"))
+(def responses 
+  (vector ">Chatbot: Hello, thanks for visiting" 
+          ">Chatbot: Good to see you again" 
+          ">Chatbot: Hi there, how can I help?"))
 
 (defn greeting
-  "Using similarity function, identifies if the user-input is in possible-greetings.
-   If true, function prints random greeting message, otherwise retruns false."
+  "Using similarity function, identifies if 
+   the user input is in possible-greetings.
+   If true, function prints random greeting message.
+   Otherwise returns false."
   [greeting-vector input]
    (let [words (parse-input input)]
      (if (not (empty? greeting-vector))
-       (let [max-similarity (apply max (for [y words] (similarity y (first greeting-vector))))]
+       (let [max-similarity 
+             (apply max 
+                    (for [y words] 
+                      (similarity y (first greeting-vector))))]
          (if (> max-similarity 0.7)
            (rand-nth responses)
            (greeting (rest greeting-vector) input)))
