bfc324c69f374670bba155617aa290d66aa64a67
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-11-21 20:11:17 +0100

    Update find_park_data.clj

--- src/chatbot/find_park_data.clj
+++ src/chatbot/find_park_data.clj
@@ -1,3 +1,6 @@
+(ns chatbot.find_park_data
+  (:require [chatbot.core :refer [parse-json]]))
+
 (defn find-park-data
   "Checks what value of the keyword identified in user input is in the data
   structure containing data about the park and prints out the corresponding
@@ -8,28 +11,42 @@
       (cond
         (some #(= found-keyword %) ["wc" "playground" "parking"])
           (if (= true park-data)
-            (println (format ">Chatbot: You can find %s in Bertramka. " found-keyword))
-            (println (format ">Chatbot: Unfortunately, there is no %s in Bertramka. " found-keyword)))
+            (format
+              ">Chatbot: You can find %s in Bertramka."
+              found-keyword)
+            (format
+              ">Chatbot: Unfortunately, there is no %s in Bertramka."
+              found-keyword))
 
         (some #(= found-keyword %) ["biking" "skating" "skiing"])
           (if (= true park-data)
-           (println (format ">Chatbot: %s is possible in Bertramka. " found-keyword))
-           (println (format ">Chatbot: Unfortunately, %s is not possible in Bertramka. " found-keyword)))
+            (format
+              ">Chatbot: %s is possible in Bertramka."
+              found-keyword)
+            (format
+              ">Chatbot: Unfortunately, %s is not possible in Bertramka." 
+              found-keyword))
 
         (= found-keyword "attractions")
-          (println (format ">Chatbot: In Bertramka you can find multiple attractions, some are: %s" park-data))
+          (format
+            ">Chatbot: In Bertramka you can find such attractions as: %s."
+            park-data)
 
         (= found-keyword "transportation")
-          (println (format ">Chatbot: You can get to Bertramka these ways:, they are: %s" park-data))
+          (format
+            ">Chatbot: You can get to Bertramka these ways:, they are: %s."
+            park-data)
 
         (= found-keyword "sports")
           (if (= true park-data)
-            (println (format ">Chatbot: There is a sport field in Bertramka."))
-            (println (format ">Chatbot: Unfortunately, there is no sport field in Bertramka.")))
+            ">Chatbot: There is a sport field in Bertramka."
+            ">Chatbot: Unfortunately, there is no sport field in Bertramka.")
 
         (= found-keyword "dogs")
           (if (= true park-data)
-            (println ">Chatbot: You can enter Bertramka with your dogs")
-            (println ">Chatbot:  Unfortunately, you can't enter Bertramka with your dogs")))
+            ">Chatbot: You can enter Bertramka with your dogs."
+            ">Chatbot: Unfortunately, you can't enter Bertramka with dogs."))
 
-      (println (format ">Chatbot: There is no information provided about %s " found-keyword)))))
+      (format
+        ">Chatbot: There is no information provided about %s."
+        found-keyword))))
