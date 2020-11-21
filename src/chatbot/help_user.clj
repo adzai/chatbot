dc0961e9154fe215196a0a8c8712744857ee2003
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-21 18:10:01 +0400

    Removed whitespaces

--- src/chatbot/help_user.clj
+++ src/chatbot/help_user.clj
@@ -1,11 +1,11 @@
-(defn help-function 
+(defn help-function
   "Describes the functionalities of Chatbot."
   []
-  ">Chatbot: The chatbot is designed to answer users' 
-   questions regarding Bertramka park. 
+  ">Chatbot: The chatbot is designed to answer users'
+   questions regarding Bertramka park.
    The bot provides information about following aspects 
-   of Bertramka: wc, attractions, biking, skating, 
-   sports field, playground, transportation and parking. 
-   Error messages are used to infrom user that 
+   of Bertramka: wc, attractions, biking, skating,
+   sports field, playground, transportation and parking.
+   Error messages are used to infrom user that
    asked questions are obscure to chatbot.
    The user can finish the conversation by typing word - finish.")
