064cb6c6c8cd390bc55f943534ba4090c50fe8c0
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-11-21 20:19:57 +0100

    Testing find-park-data function

--- test/chatbot/core_test.clj
+++ test/chatbot/core_test.clj
@@ -28,3 +28,16 @@
   (testing "Testing json-parse function"
     (is
       (map? (parse-json "data/data-en.json")))))
+
+(deftest find-park-data-test
+  (testing "Testing the keyword response function with input - wc"
+    (is
+      (= ">Chatbot: Unfortunately, there is no wc in Bertramka."
+         (find-park-data "wc")))))
+
+(deftest find-park-data-absent-keyword-test
+  (testing "Testing the keyword response function with non-existent keyword"
+    (is
+      (= ">Chatbot: There is no information provided about dogs."
+         (find-park-data "dogs")))))
+
