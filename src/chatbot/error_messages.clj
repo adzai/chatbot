057bd885a0f1c49f2b04704a08db520dd5b82714
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-21 17:43:32 +0400

    Updated formatting

--- src/chatbot/error_messages.clj
+++ src/chatbot/error_messages.clj
@@ -1,2 +1,5 @@
-(def possible-error-messages 
-  (vector ">Chatbot: Sorry, I didn't understand you, please try again!" ">Chatbot: The Bot did not understand the question, please try in different words." ">Chatbot: I am sorry, I could not understand you"))
+(def possible-error-messages
+  (vector ">Chatbot: Sorry, I didn't understand you, please try again!"
+          ">Chatbot: The Bot did not understand the question,
+           please try in different words."
+          ">Chatbot: I am sorry, I could not understand you"))
