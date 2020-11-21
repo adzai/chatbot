748e7098298091efd4223f4f67650998144f3ec7
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-21 17:57:16 +0400

    Moved error messages to core.clj

--- src/chatbot/error_messages.clj
+++
@@ -1,5 +0,0 @@
-(def possible-error-messages
-  (vector ">Chatbot: Sorry, I didn't understand you, please try again!"
-          ">Chatbot: The Bot did not understand the question,
-           please try in different words."
-          ">Chatbot: I am sorry, I could not understand you"))
