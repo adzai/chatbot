9304e97233d6501c2c3a137c823bff9a999db29b
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2020-11-22 01:35:11 +0100

    Converting found keyword to Clojure keyword format

--- src/chatbot/find_park_data.clj
+++ src/chatbot/find_park_data.clj
@@ -5,48 +5,49 @@
   "Checks what value of the keyword identified in user input is in the data
   structure containing data about the park and prints out the corresponding
   bot's answer depending on the keyword type"
-  [found-keyword]
-  (let [park-data (get (parse-json "data/Bertramka.json") found-keyword)]
+  [user-keyword]
+  (let [found-keyword (keyword user-keyword)
+        park-data (get park-map found-keyword)]
     (if-not (nil? park-data)
       (cond
-        (some #(= found-keyword %) ["wc" "playground" "parking"])
+        (some #(= found-keyword %) [:wc :playground :parking])
           (if (= true park-data)
             (format
               ">Chatbot: You can find %s in Bertramka."
-              found-keyword)
+              user-keyword)
             (format
               ">Chatbot: Unfortunately, there is no %s in Bertramka."
-              found-keyword))
+              user-keyword))
 
-        (some #(= found-keyword %) ["biking" "skating" "skiing"])
+        (some #(= found-keyword %) [:biking :skating :skiing])
           (if (= true park-data)
             (format
               ">Chatbot: %s is possible in Bertramka."
-              found-keyword)
+              user-keyword)
             (format
               ">Chatbot: Unfortunately, %s is not possible in Bertramka."
-              found-keyword))
+              user-keyword))
 
-        (= found-keyword "attractions")
+        (= found-keyword :attractions)
           (format
             ">Chatbot: In Bertramka you can find such attractions as: %s."
             park-data)
 
-        (= found-keyword "transportation")
+        (= found-keyword :transportation)
           (format
             ">Chatbot: You can get to Bertramka these ways:, they are: %s."
             park-data)
 
-        (= found-keyword "sports")
+        (= found-keyword :sports)
           (if (= true park-data)
             ">Chatbot: There is a sport field in Bertramka."
             ">Chatbot: Unfortunately, there is no sport field in Bertramka.")
 
-        (= found-keyword "dogs")
+        (= found-keyword :dogs)
           (if (= true park-data)
             ">Chatbot: You can enter Bertramka with your dogs."
             ">Chatbot: Unfortunately, you can't enter Bertramka with dogs."))
 
-      (format
+      (format 
         ">Chatbot: There is no information provided about %s."
-        found-keyword))))
+        user-keyword))))
