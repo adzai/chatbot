1be61ad6bd44e2907a1c49069eaaf8d25b28d84f
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2020-11-25 14:13:20 +0400

    Update help_user.clj

--- src/chatbot/help_user.clj
+++ src/chatbot/help_user.clj
@@ -6,6 +6,6 @@
    The bot provides information about following aspects
    of Bertramka: wc, attractions, biking, skating,
    sports field, playground, transportation and parking.
-   Error messages are used to infrom user that
+   Error messages are used to inform user that
    asked questions are obscure to chatbot.
    The user can finish the conversation by typing word - finish.")
