ff9de542b1a7a8ad99bf0c67dfb0c18bda2720dc
Adam Zaiter <zaiter.adam7@gmail.com>
2020-11-27 15:11:52 +0100

    #53: add user utilities

---
+++ src/chatbot/user_utils.clj
@@ -0,0 +1,34 @@
+(ns chatbot.user_utils
+  (:require [chatbot.bot_utils :as bot]
+            [clojure.string :as str]))
+
+; User prompt which can be later changed to user's actual name
+; after asking for it in the conversation
+(def user-prompt (ref "User> "))
+
+(defn get-user-input
+  "Prints the user prompt and returns the user's input"
+  []
+  (print @user-prompt)
+  (flush)
+  (read-line))
+
+(defn set-user-prompt!
+  "Sets the username"
+  []
+  (println (str bot/bot-prompt "Input your username or type 'skip'"))
+  (loop []
+    (print @user-prompt)
+    (flush)
+    (let [ans (str/trim (read-line))]
+      (cond
+            (> (count (str/split ans #" ")) 1)
+            (do (println "Use a 1 word username")
+                (recur))
+            (> (count ans) 15)
+            (do (println "Choose a shorter username (max 15 characters)")
+                (recur))
+            (= "skip" (str/lower-case ans))
+            nil
+            :else (dosync (ref-set user-prompt (str ans "> "))))))
+  nil)
