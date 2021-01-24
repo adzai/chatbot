e73c84281844e2ae7bddf2d8a8800708031dc729
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2021-01-25 00:26:28 +0400

    Enhancement of user experience
    The user experience was enhanced by creating bird-helper function, raising errors when user input is incorrect,  and adding more info to the decision tree.
    The keyword to exit the bird identification mode was defined.

--- src/chatbot/decision_tree.clj
+++ src/chatbot/decision_tree.clj
@@ -48,184 +48,233 @@
 (defn find-node-response
   "Returns a nodes which corresponds to the user's response or nil."
   [user-response lst-of-nodes]
+  (let [node (first lst-of-nodes)]
   (cond
     (nil? (first lst-of-nodes))
     nil
-    (some #(= (:answer-to-previous (first lst-of-nodes)) %) user-response)
-       (first lst-of-nodes)
+    (some #(= (:answer-to-previous node) %) user-response)
+    node
     :else
-    (recur user-response (rest lst-of-nodes))))
+    (recur user-response (rest lst-of-nodes)))))
 
 ; Example of constructing a decision tree about birds
 (def bird-decision-tree (make-tree))
 ;bird seen or not?
 (tree-insert! bird-decision-tree nil
-              "Have you seen bird in the park? Type 'yes' or 'no'.")
-(tree-insert! bird-decision-tree "no" "Okay,bye"
+              "Have you seen bird in the park?")
+(tree-insert! bird-decision-tree "no"
+               (str
+                "For more information about birds, type - 'bird'."
+                "Otherwise, you will continue getting information"
+                "about the chosen park.")
             :attach-to
-              "Have you seen bird in the park? Type 'yes' or 'no'.")
+              "Have you seen bird in the park?")
 ;dark or light colored bird
 (tree-insert! bird-decision-tree "yes"
-              "Was the bird dark or light colored? Type 'dark' or 'light'."
+              "Was the bird dark or light colored?"
             :attach-to
-              "Have you seen bird in the park? Type 'yes' or 'no'.")
+              "Have you seen bird in the park?")
 
 ;dark colored bird - black?
 (tree-insert! bird-decision-tree "dark"
-              "Was the bird black? Type 'yes' or 'no'."
+              "Was the bird black?"
             :attach-to
-              "Was the bird dark or light colored? Type 'dark' or 'light'.")
+              "Was the bird dark or light colored?")
 ;dark or light colored beak of the black bird
 (tree-insert! bird-decision-tree "yes"
-              (str "Was black bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'.")
+              "Was black bird's beak dark or light colored?"
             :attach-to
-              "Was the bird black? Type 'yes' or 'no'.")
+              "Was the bird black?")
 ;decision made for black bird with dark beak.
 (tree-insert! bird-decision-tree "dark"
               (str "It was probably a crow. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-              (str "Was black bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'."))
+              "Was black bird's beak dark or light colored?")
 ;decision made for black bird with light beak.
 (tree-insert! bird-decision-tree "light"
               (str "It was probably a starling. "
                    "For more information about birds, "
                    "please type - 'bird' again." )
             :attach-to
-              (str "Was black bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'."))
+              "Was black bird's beak dark or light colored?")
 ;dark colored bird - brown?
 (tree-insert! bird-decision-tree "no"
-              "Was the bird brown? Type 'yes' or 'no'."
+              "Was the bird brown?"
             :attach-to
-              "Was the bird black? Type 'yes' or 'no'.")
+              "Was the bird black?")
 ;dark or light colored beak of the brown bird
 (tree-insert! bird-decision-tree "yes"
-              (str "Was brown bird's beak dark or light colored? "
-                    "Type 'dark' or 'light'.")
-            :attach-to "Was the bird brown? Type 'yes' or 'no'.")
+              "Was brown bird's beak dark or light colored?"
+            :attach-to "Was the bird brown?")
 ;decision made for brown bird with dark beak.
 (tree-insert! bird-decision-tree "dark"
               (str "It could have been a european dipper. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-              (str "Was brown bird's beak dark or light colored? "
-                    "Type 'dark' or 'light'."))
+              "Was brown bird's beak dark or light colored?")
 ;decision made for brown bird with light beak.
 (tree-insert! bird-decision-tree "light"
               (str "It was probably a european greenfinch. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-             (str "Was brown bird's beak dark or light colored? "
-                    "Type 'dark' or 'light'."))
+             "Was brown bird's beak dark or light colored?")
 ;dark colored bird - grey?
 (tree-insert! bird-decision-tree "no"
-              "Was the bird grey? Type 'yes' or 'no'."
+              "Was the bird grey?"
             :attach-to
-              "Was the bird brown? Type 'yes' or 'no'.")
+              "Was the bird brown?")
 ;dark or light colored beak of the grey bird
 (tree-insert! bird-decision-tree "yes"
-              (str "Was grey bird's beak dark or light colored? "
-                    "Type 'dark' or 'light'.")
+              "Was grey bird's beak dark or light colored?"
             :attach-to
-              "Was the bird grey? Type 'yes' or 'no'.")
+              "Was the bird grey?")
 ;decision made for grey bird with dark beak.
 (tree-insert! bird-decision-tree "dark"
               (str "I think it was a grey catbird. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-              (str "Was grey bird's beak dark or light colored? "
-                    "Type 'dark' or 'light'."))
+              "Was grey bird's beak dark or light colored?")
 ;decision made for grey bird with light beak.
 (tree-insert! bird-decision-tree "light"
               (str "It was probably a nuthatch. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-              (str "Was grey bird's beak dark or light colored? "
-                    "Type 'dark' or 'light'."))
+              "Was grey bird's beak dark or light colored?")
 
 
 ;light colored bird - red?
 (tree-insert! bird-decision-tree "light"
-              "Was the bird red? Type 'yes' or 'no'."
+              "Was the bird red?"
             :attach-to
-              "Was the bird dark or light colored? Type 'dark' or 'light'.")
+              "Was the bird dark or light colored?")
 ;dark or light colored beak of the red bird
 (tree-insert! bird-decision-tree "yes"
-              (str "Was red bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'.")
+              "Was red bird's beak dark or light colored?"
             :attach-to
-              "Was the bird red? Type 'yes' or 'no'.")
+              "Was the bird red?")
 ;decision made for red bird with dark beak.
 (tree-insert! bird-decision-tree "dark"
               (str "It was probably a robin. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-             (str "Was red bird's beak dark or light colored? "
-                  "Type 'dark' or 'light'."))
+              "Was red bird's beak dark or light colored?")
 ;decision made for red bird with light beak.
 (tree-insert! bird-decision-tree "light"
               (str "It could have been a middle spotted woodpecker. "
                    "For more information about birds, "
                    "please type - 'bird' again.")
             :attach-to
-              (str "Was red bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'."))
+              "Was red bird's beak dark or light colored?")
 ;light colored bird - white?
 (tree-insert! bird-decision-tree "no"
-              "Was the bird white? Type 'yes' or 'no'."
+              "Was the bird white?"
             :attach-to
-              "Was the bird red? Type 'yes' or 'no'.")
+              "Was the bird red?")
 ;dark or light colored beak of the white bird
 (tree-insert! bird-decision-tree "yes"
-              (str "Was white bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'.")
+              "Was white bird's beak dark or light colored?"
             :attach-to
-              "Was the bird white? Type 'yes' or 'no'.")
+              "Was the bird white?")
 ;decision made for white bird with dark beak.
 (tree-insert! bird-decision-tree "dark"
-             (str "I assume it was a marsh tit. "
-                  "For more information about birds, "
-                  "please type - 'bird' again.")
-              :attach-to
-              (str "Was white bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'."))
+              (str "I assume it was a marsh tit. "
+                   "For more information about birds, "
+                   "please type - 'bird' again.")
+            :attach-to
+              "Was white bird's beak dark or light colored?")
 ;decision made for white bird with light beak.
 (tree-insert! bird-decision-tree "light"
               (str "It was probably a collared dove."
                    "For more information about birds, "
                    "please type - 'bird' again.")
-            :attach-to
-              (str "Was white bird's beak dark or light colored? "
-                   "Type 'dark' or 'light'."))
+             :attach-to
+              "Was white bird's beak dark or light colored?")
+;match not found for dark colored bird, i.e.,
+; the seen bird is not black, brown or grey.
+(tree-insert! bird-decision-tree "no"
+              (str "Unfortunately, the match was not found."
+                   "For more information about birds, type - 'bird'.")
+              :attach-to
+               "Was the bird grey?")
+;match not found for light colored bird, i.e.,
+; the seen bird is not red or white.
+(tree-insert! bird-decision-tree "no"
+              (str "Unfortunately, the match was not found. "
+                   "For more information about birds, type - 'bird'.")
+              :attach-to
+               "Was the bird white?")
 
+
+
+;bird-helper
+(defn bird-helper []
+  (bot-print! (str
+               "The bot can help the user to identify "
+               "the birds of Prague parks."))
+  (bot-print! (str
+               "Bot asks user questions about the"
+               "specific characteritics of birds."))
+  (bot-print! (str
+               "Based on the recieved inputs, "
+                "chatbot will identifiy the bird species."))            
+  (bot-print!  (str
+                "The user must give the specific inputs to the asked questions"
+                ", for example, 'yes'/'no' or 'dark'/'light'." ))
+  (bot-print!  (str
+               "If the user-input is obscure to the bot, "
+                "it will raise an error and ask user "
+                "for more specific response."))
+  (bot-print!  (str
+                "Once the bird is identified, the bot goes to the normal mode "
+                "and recieves user input about the chosen park."))
+  (bot-print!  (str
+               "If the bird is not identified, the bot informs the user "
+               "that the match is not found and goes to the normal mode."))
+   (bot-print!  (str
+               "The user can exit the bird identifier mode by typing "
+               "keyword - 'helper'."))
+  (bot-print!  (str
+               "In order to continue bird identification, type the "
+               "keyword - 'bird' once again.")))
 
 ; Example of how the decision tree traversal with user input might look like
- (defn questions-loop-helper
-   [node]
-   (bot-print! (:response-to-user node))
-   (when-not (empty? @(:children node))
-     (let [user-input (parse-input (get-user-input))
-          next-node (find-node-response user-input @(:children node))]
-       (if (nil? next-node)
-          (do
-            (bot-print!  "Couldn't find a match for your answer.")
-            (bot-print!  (str
-                          "For more information about birds,"
-                          "type - 'bird'."))
-            (bot-print!  (str
-                          "Otherwise, you can continue to get information "
-                          "about the current park "
-                          "or change the park by typing keyword - 'park'.")))
-         (recur next-node)))))
+ (defn questions-loop-helper [node]
+   (if (empty? @(:children node))
+     (bot-print! (:response-to-user node))
+     (do (bot-print! (:response-to-user node))
+         (let [user-input (parse-input (get-user-input))
+               next-node (find-node-response user-input @(:children node))
+               bird-help? (= '("helper") user-input)
+               bird-exit? (= '("exit") user-input)]
+           (cond
+             bird-exit?
+             (do
+               (bot-print!
+                "Now, you can ask questions about the chosen park.")
+               (bot-print!
+                "To change the current park, type - 'park'."))
+
+             bird-help?
+             (bird-helper)
+
+             (nil? next-node)
+             (do
+               (bot-print!
+                (str
+                 "Please, give a specific answer, for example, "
+                 "'yes'/'no' or 'dark'/'light'. "))
+               (bot-print!
+                "If you need further help, type - 'helper'.")
+               (questions-loop-helper node))
+
+             :else (recur next-node))))))
 
  (defn questions-loop
    [tree]
