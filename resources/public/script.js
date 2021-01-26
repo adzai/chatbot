b122b251c07a8c15ddbfa4429421f97addf51e3a
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2021-01-26 16:32:12 +0100

    Script to fix chat window visibility

--- resources/public/script.js
+++ resources/public/script.js
@@ -2,3 +2,12 @@ window.onload=function () {
      var objDiv = document.getElementById("chat-window");
      objDiv.scrollTop = objDiv.scrollHeight;
 }
+
+
+const chatWindowHeight = document.querySelector('#chat-window');
+const chatTitleHeight = document.querySelector('.chat-title');
+const chat = document.querySelector('.chat');
+
+if (chatWindowHeight.scrollHeight == 0) {
+     chat.style.visibility = "hidden";
+}
