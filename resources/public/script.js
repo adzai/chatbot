263a2031fd289bf5ff9a775b2de687c67aa52896
irinakulinich3712 <44743819+irinakulinich3712@users.noreply.github.com>
2021-01-26 21:13:32 +0100

    Updated script

--- resources/public/script.js
+++ resources/public/script.js
@@ -1,13 +1,10 @@
-window.onload=function () {
-     var objDiv = document.getElementById("chat-window");
-     objDiv.scrollTop = objDiv.scrollHeight;
-}
-
-
-const chatWindowHeight = document.querySelector('#chat-window');
-const chatTitleHeight = document.querySelector('.chat-title');
-const chat = document.querySelector('.chat');
-
-if (chatWindowHeight.scrollHeight == 0) {
-     chat.style.visibility = "hidden";
+window.onload = () => {
+     const chatWindow = document.querySelector('#chat-window');
+     const chat = document.querySelector('.chat');
+     if (chatWindow != null && chatWindow.scrollHeight == 0) {
+          chat.style.visibility = "hidden";
+     }
+     if (chatWindow != null) {
+          chatWindow.scrollTop = chatWindow.scrollHeight;
+     }
 }
