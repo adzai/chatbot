8b8840b6def608f12a4be8d619bfd6721237a7aa
irinakulinich3712 <iryna.kulinich@praguecollege.cz>
2021-01-26 13:25:36 +0100

    Changing html for styling

--- src/web/pages.clj
+++ src/web/pages.clj
@@ -17,14 +17,16 @@
     [:body
      [:div {:id "content"}
       [:h1 "Prague parks chatbot"]
-      [:p [:a {:href "/"} "Home"]]
+      [:p {:class "btn-home-wrapper"} [:a {:href "/" :class "btn btn-home"} "Home"]]
       [:h3 (str "Current park: " (park/keyword->park
                                    (str/replace current-uri #"/" "")))]
-      [:div {:id "chat-window"}
-       content]
       [:form {:method "POST" :action current-uri}
        [:input
-        {:type "text" :autofocus "autofocus" :id "input" :name "input"}]]]]))
+        {:type "text" :autofocus "autofocus" :id "input" :name "input"}]]
+      [:div {:class "chat"}
+      [:div {:class "chat-title"} "Park bot chat"]
+      [:div {:id "chat-window"}
+         content]]]]))
 
 (defn index
   "Home page"
@@ -37,36 +39,37 @@
     [:body
      [:h1 "Prague parks chatbot"]
      [:h2 "List of parks: "]
-     [:p
-      [:a {:href "/bertramka"}
-       "Bertramka"]]
-     [:p
-      [:a {:href "/riegerovy-sady"}
-       "Riegerovy Sady"]]
-     [:p
-      [:a {:href "/klamovka"}
-       "Klamovka"]]
-     [:p
-      [:a {:href "/letna"}
-       "Letná"]]
-     [:p
-      [:a {:href "/stromovka"}
-       "Stromovka"]]
-     [:p
-      [:a {:href "/ladronka"}
-       "Ladronka"]]
-     [:p
-      [:a {:href "/frantiskanska-zahrada"}
-       "Františkánská zahrada"]]
-     [:p
-      [:a {:href "/kampa"}
-       "Kampa"]]
-     [:p
-      [:a {:href "/petrin"}
-       "Petřín"]]
-     [:p
-      [:a {:href "/kinskeho-zahrada"}
-       "Kínského zahrada"]]
-     [:p
-      [:a {:href "/vysehrad"}
-       "Vyšehrad"]]]))
+     [:div {:class "park-list"}
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/bertramka" :class "btn btn-park"}
+         "Bertramka"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/riegerovy-sady" :class "btn btn-park"}
+         "Riegerovy Sady"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/klamovka" :class "btn btn-park"}
+         "Klamovka"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/letna" :class "btn btn-park"}
+         "Letná"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/stromovka" :class "btn btn-park"}
+         "Stromovka"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/ladronka" :class "btn btn-park"}
+         "Ladronka"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/frantiskanska-zahrada" :class "btn btn-park"}
+         "Františkánská zahrada"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/kampa" :class "btn btn-park"}
+         "Kampa"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/petrin" :class "btn btn-park"}
+         "Petřín"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/kinskeho-zahrada" :class "btn btn-park"}
+         "Kínského zahrada"]]
+       [:p {:class "btn-wrapper"}
+        [:a {:href "/vysehrad" :class "btn btn-park"}
+         "Vyšehrad"]]]]))
