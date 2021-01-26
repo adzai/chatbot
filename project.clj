7600d46e4c4fd2796ca1e145a67ae1196b555df8
Adam Zaiter <zaiter.adam7@gmail.com>
2021-01-26 22:20:25 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chatbot "0.2.0"
+(defproject chatbot "1.0.0"
   :description "A clojure chatbot focused on parks in Prague"
   :url "https://adzai.github.io/chatbot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -25,5 +25,5 @@
   :main chatbot.core
   :repl-options {:init-ns chatbot.core}
   :profiles
-    {:uberjar {:env {:production true}
-               :aot :all}})
+    {:uberjar
+     {:aot :all}})
